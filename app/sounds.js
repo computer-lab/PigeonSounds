87d8443619ccadd996457b8db392faeac92ab1e4
Daniel Brewster <dnbrwstr@gmail.com>
2018-01-25 20:14:16 -0500

    Update sound data

--- app/sounds.js
+++ app/sounds.js
@@ -1,31 +1,31 @@
 const sounds = [
   {
     label: "Alarmed Takeoff",
-    filename: "Alarmed_Takeoff.mp3"
+    filename: "alarmed_takeoff.mp3"
   },
   {
     label: "Broody Grunts and Pecks",
-    filename: "Broody_Grunts_and_Pecks.mp3"
+    filename: "broody_grunts_and_pecks.mp3"
   },
   {
     label: "Happy Grunts",
-    filename: "Happy_Grunts.mp3"
+    filename: "happy_grunts.mp3"
   },
   {
     label: "Indignant Wing Slaps",
-    filename: "Indigant_Wing_Slaps.mp3"
+    filename: "indigant_wing_slaps.mp3"
   },
   {
     label: "Nest Call",
-    filename: "Nest_Call.mp3"
+    filename: "nest_call.mp3"
   },
   {
     label: "Squeaker",
-    filename: "Squeaker.mp3"
+    filename: "squeaker.mp3"
   },
   {
     label: "Strutting Call",
-    filename: "Strutting_Call.mp3"
+    filename: "strutting_call.mp3"
   }
 ];
 
